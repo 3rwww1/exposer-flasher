8866b889f2e4b3e289fd09c29f1bbb7c4eeb9edf
dcfvg <qwsx@dcfvg.com>
2016-01-03 17:31:37 +0100

    cleaning up again

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var http = require('http');
 
-// app settings
+//
+// express settings
+//
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -18,19 +19,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'content')));
 
-// server setting
+//
+// http server
+//
 var port = 3000;
 app.set('port', port);
 
 var server = http.createServer(app);
-
 server.listen(port);
 server.on('listening', onListening);
 
 var io = require('socket.io')(server);
 
-// routes setting
-
+//
+// routes
+//
 app.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -40,14 +43,18 @@ app.get('/projection/', function(req, res, next) {
 });
 
 app.get('/flash/:state/',function(req, res){
-
   io.sockets.emit('flash', JSON.parse(req.params.state));
   res.send('Hello World!\n');
 })
 
-// lanch main
+//
+// lanch manager
+//
 require('./manager')(app, io);
 
+//
+// utils
+//
 function onListening() {
   var addr = server.address();
   var bind = typeof addr === 'string'
