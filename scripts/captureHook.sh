723491c791db946d5ba7ad0cf61d7a53096e3fdf
dcfvg <qwsx@dcfvg.com>
2016-01-11 14:30:07 +0100

    scripts hook + clientsinit

---
+++ scripts/captureHook.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+case "$ACTION" in
+    init)
+
+        ;;
+    start)
+        echo "$self: START"
+        ;;
+    download)
+        curl http://localhost:3000/captureEnd/;
+        echo "$self: DOWNLOAD to $ARGUMENT"
+        ;;
+    stop)
+        echo "$self: STOP"
+        ;;
+    *)
+        echo "$self: Unknown action: $ACTION"
+        ;;
+esac
+
+exit 0
---
+++ scripts/clientsInit.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# killall -9 "Safari"
+killall -9 "Google Chrome"
+
+sleep 3
+
+osascript <<EOF
+  tell application "Google Chrome"
+    open location "http://localhost:3000/projection"
+    activate
+
+    delay 2
+
+    get bounds of first window
+    set bounds of first window to {2500, 0, 3500, 1000}
+
+    tell window 1 to enter presentation mode
+  end tell
+EOF
+
+
+osascript <<EOF
+  tell application "Chromium"
+    open location "http://localhost:3000/monitor"
+    activate
+
+    delay 2
+
+    get bounds of first window
+    set bounds of first window to {0, 0, 1000, 1000}
+
+    tell window 1 to enter presentation mode
+  end tell
+EOF
