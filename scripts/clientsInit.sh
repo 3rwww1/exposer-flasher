b99195dca5cca89c750ace62bb365b5d70159f02
dcfvg <qwsx@dcfvg.com>
2016-01-13 18:17:45 +0100

    client windows started together

--- scripts/clientsInit.sh
+++ scripts/clientsInit.sh
@@ -1,38 +1,22 @@
 #!/bin/sh
 
-# killall -9 "Safari"
-killall -9 "Google Chrome"
-killall -9 "Chromium"
-
-sleep 2
-
 osascript <<EOF
   tell application "Chromium"
     open location "http://localhost:3000/projection"
-    activate
 
-    delay 2
+    delay 1
 
-    get bounds of first window
     set bounds of first window to {2500, 0, 3500, 1000}
-
     tell window 1 to enter presentation mode
 
   end tell
-EOF
 
-sleep 1
-
-osascript <<EOF
   tell application "Google Chrome"
     open location "http://localhost:3000/monitor"
-    activate
 
-    delay 2
+    delay 1
 
-    get bounds of first window
     set bounds of first window to {0, 0, 1000, 1000}
-
     tell window 1 to enter presentation mode
 
   end tell
