652dcea7bd8db13aa45d2df5fa3a60fc8c5b3888
dcfvg <qwsx@dcfvg.com>
2016-01-19 19:50:04 +0100

    invert screens

--- scripts/clientsInit.sh
+++ scripts/clientsInit.sh
@@ -6,7 +6,7 @@ osascript <<EOF
 
     delay 1
 
-    set bounds of first window to {2500, 0, 3500, 1000}
+    set bounds of first window to {0, 0, 1000, 1000}
     tell window 1 to enter presentation mode
 
   end tell
@@ -16,7 +16,7 @@ osascript <<EOF
 
     delay 1
 
-    set bounds of first window to {0, 0, 1000, 1000}
+    set bounds of first window to {2500, 0, 3500, 1000}
     tell window 1 to enter presentation mode
 
   end tell
