f43b84e4febdb87bf6656f08944c74639ba7bfc5
dcfvg <qwsx@dcfvg.com>
2016-01-29 10:02:11 +0100

    vlc lancher

--- scripts/initLiveVlc.sh
+++ scripts/initLiveVlc.sh
@@ -2,4 +2,4 @@
 
 dir=${0%/*}
 
-/Applications/VLC.app/Contents/MacOS/VLC --fullscreen --no-osd --no-video-title-show --video-on-top --no-media-library --repeat $1
+/Applications/VLC.app/Contents/MacOS/VLC --fullscreen --no-osd --no-video-title-show --video-on-top --no-media-library --loop $1 $1 $1 $1
