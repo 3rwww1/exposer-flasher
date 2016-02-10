c8bf515bb042978a7076346685234bae64aadaa0
dcfvg <qwsx@dcfvg.com>
2016-02-10 10:14:52 +0100

    prevent from flikering

--- public/js/monitor.js
+++ public/js/monitor.js
@@ -7,15 +7,19 @@ function init() {
 
   var player = document.getElementById("live");
 
-  $('#live').bind('ended',function(){
-    refreshLive();
-  });
+  $('#live').bind('ended',function(){ refreshLive(); });
 
   refreshLive();
 
   function refreshLive(){
-    $('#live').attr("src",liveUrl+ "?" + Math.round((new Date()).getTime() / 1000)).load();
-    $('#live').get(0).play();
+
+    $('#live').hide();
+
+    setTimeout(function(){
+      $('#live').show();
+      $('#live').attr("src",liveUrl+ "?" + Math.round((new Date()).getTime() / 1000)).load();
+      $('#live').get(0).play();
+    }, 1500 )
   };
 };
 $(document).on('ready', init);
